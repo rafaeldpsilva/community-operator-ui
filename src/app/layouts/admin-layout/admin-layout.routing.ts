6fc57c32f685200f879d68d1bd4aa0c0550d6d8f
rafaeldpsilva <1190979@isep.ipp.pt>
2023-06-24 15:35:20 +0100

    delete unused

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -2,18 +2,10 @@ import { Routes } from '@angular/router';
 
 import { DashboardComponent } from '../../pages/dashboard/dashboard.component';
 import { DemandResponseComponent } from '../../pages/demandresponse/demandresponse.component';
-import { UserComponent } from '../../pages/user/user.component';
 import { CommunityComponent } from '../../pages/community/community.component';
-import { TypographyComponent } from '../../pages/typography/typography.component';
-import { IconsComponent } from '../../pages/icons/icons.component';
-import { NotificationsComponent } from '../../pages/notifications/notifications.component';
 
 export const AdminLayoutRoutes: Routes = [
     { path: 'dashboard',      component: DashboardComponent },
     { path: 'demandresponse',      component: DemandResponseComponent },
     { path: 'community',          component: CommunityComponent }
-    //{ path: 'user',           component: UserComponent },
-    //{ path: 'typography',     component: TypographyComponent },
-    //{ path: 'icons',          component: IconsComponent },
-    //{ path: 'notifications',  component: NotificationsComponent }
 ];
