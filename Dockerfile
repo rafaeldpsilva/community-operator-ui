25d0da1cc27e35cdc84432451f47790f44cca2d3
rafaeldpsilva <1190979@isep.ipp.pt>
2023-06-24 15:36:59 +0100

    old browser

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM node:latest as node
 WORKDIR /app
 COPY . .
 RUN npm install --legacy-peer-deps
-RUN npm run build --prod
+RUN npm run build --omit=dev
 FROM nginx:alpine
 COPY --from=node /app/dist /usr/share/nginx/html
--- docker.sh
+++ docker.sh
@@ -1,28 +1,8 @@
 #!/bin/bash
-# Input version
-input_version=$(docker images | grep 'v0*' | awk '{print $2}')
-
-echo "Original version: $input_version"
-input_version=$(echo "$input_version" | tr -d '\n')
-input_version=${input_version#v}
-
-# Extract major and minor version numbers
-major_version=$(echo "$input_version" | cut -d. -f1)
-minor_version=$(echo "$input_version" | cut -d. -f2)
-
-# Convert minor version to a floating-point number
-minor_version_float=$(echo "scale=1; $minor_version + 1" | bc)
-
-# Format the new version number
-new_version="v${major_version}.${minor_version_float}"
-
-echo "Updated version: $new_version"
 
 docker rmi rdpds/tiocps-community-operator-ui:latest
-docker rmi rdpds/tiocps-community-operator-ui:v$input_version
 docker build -t rdpds/tiocps-community-operator-ui .
 id=$(docker images | grep 'rdpds/tiocps-community*' | head -1 | awk '{print $3}')
 echo "ID: $id"
-docker tag $id rdpds/tiocps-community-operator-ui:$new_version
 docker tag $id rdpds/tiocps-community-operator-ui:latest
 docker push --all-tags rdpds/tiocps-community-operator-ui
