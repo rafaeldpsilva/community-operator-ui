23d80bb9007ae155a821d0f41ab2692729448450
rafaeldpsilva <1190979@isep.ipp.pt>
2024-01-29 17:00:55 +0000

    graph update

--- src/views/components/Dashboard/EnergyChart.vue
+++ src/views/components/Dashboard/EnergyChart.vue
@@ -1,7 +1,17 @@
 <template>
-  <div class="card">
-    <div class="pb-0 card-header mb-0">
-      <h6>{{ title }}</h6>
+    <div class="card">
+        <div class="pb-0 card-header mb-0">
+            <h6>{{ title }}</h6>
+            <p v-if="loading" class="text-sm">
+            <i class="fas fa-circle-notch fa-spin"></i>
+            <span class="font-weight-bold">{{ detail1 }}</span>
+            </p>
+        </div>
+        <div class="p-3 card-body">
+            <div class="chart">
+                <v-chart class="chart" :option="option" autoresize />
+            </div>
+        </div>
     </div>
     <div class="p-3 card-body">
       <div class="chart">
@@ -11,94 +21,97 @@
   </div>
 </template>
 <script>
-import { use } from 'echarts/core';
-import { CanvasRenderer } from 'echarts/renderers';
-import { LineChart } from 'echarts/charts';
-import {
-  LegendComponent,
-  GridComponent,
-} from 'echarts/components';
-import VChart, { THEME_KEY } from 'vue-echarts';
-import { ref, defineComponent } from 'vue';
-import DashboardService from "../../../services/dashboard/DashboardService.js";
+    import { use } from 'echarts/core';
+    import { CanvasRenderer } from 'echarts/renderers';
+    import { LineChart } from 'echarts/charts';
+    import {
+        LegendComponent,
+        GridComponent,
+    } from 'echarts/components';
+    import VChart, { THEME_KEY } from 'vue-echarts';
+    import { ref, defineComponent } from 'vue';
+    import DashboardService from "../../../services/dashboard/DashboardService.js";
 
-use([
-  CanvasRenderer,
-  LineChart,
-  GridComponent,
-  LegendComponent
-]);
+    use([
+        CanvasRenderer,
+        LineChart,
+        GridComponent,
+        LegendComponent
+    ]);
 
-export default defineComponent({
-  components: {
-    VChart,
-  },
-  props: {
-    title: {
-      type: String,
-      default: "Energy Overview",
-    },
-    detail1: {
-      type: String,
-      default: "4% more",
-    },
-    detail2: {
-      type: String,
-      default: "in 2021",
-    },
-  },
-  data() {
-    return {
-      loading: true,
-    }
-  },
-  provide: {
-    [THEME_KEY]: 'light',
-  },
-  mounted() {
-    this.loadOverview();
-  },
-  methods: {
-    async loadOverview() {
-      let overview = await DashboardService.getEnergyHistoric();
-      this.option.xAxis.data = overview[0]
-      this.option.series[0].data = overview[1];
-      this.option.series[1].data = overview[2];
-      this.option.series[2].data = overview[3];
-    },
-  },
-  setup() {
-    const option = ref({
-      tooltip: {
-        trigger: 'axis'
-      },
-      legend: {
-        data: ['Consumption', 'Generation', 'Flexibility']
-      },
-      grid: {
-        left: '3%',
-        right: '4%',
-        bottom: '3%',
-        containLabel: true
-      },
-      xAxis: {
-        type: 'category',
-        boundaryGap: false,
-        data: [0]
-      },
-      yAxis: {
-        type: 'value'
-      },
-      series: [
-        {
-          name: 'Consumption',
-          type: 'line',
-          data: [0]
+    export default defineComponent({
+        components: {
+            VChart,
         },
-        {
-          name: 'Generation',
-          type: 'line',
-          data: [0]
+        props: {
+            title: {
+                type: String,
+                default: "Energy Overview",
+            },
+        },
+        data() {
+            return {
+                loading: true,
+                detail1: " Loading...",
+            }
+        },
+        provide: {
+            [THEME_KEY]: 'light',
+        },
+        mounted() {
+            this.loadOverview();
+        },
+        methods: {
+            async loadOverview() {
+                let overview = await DashboardService.getEnergyHistoric();
+                this.option.xAxis.data = overview[0]
+                this.option.series[0].data = overview[1];
+                this.option.series[1].data = overview[2];
+                this.option.series[2].data = overview[3];
+                this.loading = false;
+            },
+        },
+        setup() {
+            const option = ref({
+                tooltip: {
+                    trigger: 'axis'
+                },
+                legend: {
+                    data: ['Consumption', 'Generation', 'Flexibility']
+                },
+                grid: {
+                    left: '3%',
+                    right: '4%',
+                    bottom: '3%',
+                    containLabel: true
+                },
+                xAxis: {
+                    type: 'category',
+                    boundaryGap: false,
+                    data: [0]
+                },
+                yAxis: {
+                    type: 'value'
+                },
+                series: [
+                    {
+                        name: 'Consumption',
+                        type: 'line',
+                        data: [0]
+                    },
+                    {
+                        name: 'Generation',
+                        type: 'line',
+                        data: [0]
+                    },
+                    {
+                        name: 'Flexibility',
+                        type: 'line',
+                        data: [0]
+                    }
+                ]
+            });
+            return { option };
         },
         {
           name: 'Flexibility',
@@ -114,6 +127,6 @@ export default defineComponent({
 
 <style scoped>
 .chart {
-  height: 40vh;
+    height: 40vh;
 }
 </style>
